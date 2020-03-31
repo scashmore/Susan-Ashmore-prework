9c3394c06a6fcd9eb7fca61cf5a385795f468083
Susan Ashmore <scashmorecitcom@gmail.com>
2020-03-31 17:15:32 -0400

    java project

--- Module-2_Assessment/assests/javascript/game.js
+++ Module-2_Assessment/assests/javascript/game.js
@@ -1,65 +1,138 @@
-java.util.Scanner;
-java.util.Random;
+//letters
+var letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//Nintendo Switch Titles and Images
+var titles = ['zleda', 'odyssey', 'animalcrossing', 'mariokart', 'luigismansion', 'pokemon', 'splatoon', 'smashbros', 'xenoblade', 'yoshiscraftedworld', 'hollowknight', 'kirby', 'donkeykong', 'fireemblem'];
+//chosen title
+var chosentitle = "";
+var chosenletters = [];
+var underscores = 0;
+//Correct
+var underright =[];
+//Wrong
+var wrongletters = [];
+//Counter
+var correctguesscount = 0;
+var wincount = 0;
+var losecount = 0;
+var guessesleft = 10;
+function reset()
+{
+	//Chooses random title
+	chosentitle = titles[Math.floor(Math.random() * titles.length)];
+	chosenletters = chosentitle.split('');
+    underscores = chosenletters.length;
+    
+    //Reset
+    guessedletters = 0;
+    correctguesscount = 0;
+    guessesleft = 10;
+    wrongletters = [];
+    underright = [];
+    letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    test = false; 
+    start();
+    //Debug
+    console.log(chosentitle);
+    console.log(chosenletters);
+    console.log(underscores);
+}
+function start()
+{
+    //Random title
+    chosentitle = titles[Math.floor(Math.random() * titles.length)];
+	chosenletters = chosentitle.split('');
+    underscores = chosenletters.length; 
+    //Reset
+    correctguesscount = 0;
+    guessesleft = 10;
+    wrongletters = [];
+    underright = [];
+    letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    //Fill underscore
+    for(var i = 0; i < underscores; i++)
+    {
+    underright.push('_');
+    document.getElementById('underscore').innerHTML = underright.join(' ');
+    }
+    //Debug
+    console.log(chosentitle);
+    console.log(chosenletters);
+    console.log(underscores);
+    console.log(underright);
+}
+//HTML
+document.getElementById('underscore').innerHTML = underright.join(' ');
+document.getElementById('lives').innerHTML = guessesleft;
+document.getElementById('wins').innerHTML = wincount;
+document.getElementById('fails').innerHTML = losecount;
+document.getElementById('wrongletters').innerHTML = wrongletters;
 
-Public class Hangman {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        String[] guesses = {"Legend of Zelda Breath of the Wild", "Super Mario Odyssey", "Animal Crossing New Horizons", "Mario Kart", "Luigis Mansion", "Pokemon Sword and Sheild", "Splatoon", "Supper Smash Bros Ultimate", "Xenoblade Chronicle", "Yoshis Crafted World", "Hollow Knigh", "Kirby Star Allies", "Donkey Kong Country Tropical Freeze", "Fire Emblem Three Houses" };
-
-        boolean Currently_playing = true
-        while (Currently_playing) {
-            System.out.println("Nitento Titles Hangman: Switch Edition");
-            char[] Random_title = guesses[random.nextInt(guesses.length)].toCharArray();
-            int Number_of_guesses = 12;
-            char[] Player_guess = new char[Number_of_guesses];
-
-            for (int i = 0; i < Player_guess.length; i++) {
-                Player_guess[i] = '_';
-
-            }
-        boolean Title_guessed = false;
-        int tries = 0;
-
-        while (!Random_title && tries != Number_of_guesses) {
-            System.out.print("Number of Guesses: ");
-            printArray(Player_guess);
-            System.out.printf("%d tries remain \n", Number_of_guesses - tries);
-            System.out.println("Enter a charachter");
-            char input = scanner.nextLine().charAt(0);
-            tries++;
-
-            if (input =='_') {
-                Currently_playing = false;
-                Title_guessed = true;
-            } else {
-                for (int i = 0; i < 12; i++) {
-                    Player_guess[i] = input;
-                }
-            }
-            if (Title_guessed(Player_guess)) {
-                Title_guessed = true;
-                System.out.println("You've won!");
+function compareletters(userKey)
+{   console.log('Am I working?')
+    if(chosenletters.indexOf(userKey) > -1)
+    {
+        for(var i = 0; i < underscores; i++)
+        {
+            if(chosenletters[i] === userKey)
+            {
+            correctguesscount++;
+            underright[i] = userKey;
+            document.getElementById('underscore').innerHTML = underright.join(' ');
             }
         }
-        }
-        if (!Title_guessed) System.out.println("You ran out of tires :/");
-        System.out.println("Replenish your lives and play again? (yes/no");
-        String playagain = scanner.nextLine();
-        if (playagain.equals(no)) Currently_playing = false;
+        //Debug
+        console.log(underright);
     }
-    System.out.prinln("Game Over");
+    //Wrong key
+    else
+    {
+        wrongletters.push(userKey);
+        guessesleft--;
+        document.getElementById('lives').innerHTML = guessesleft;
+        document.getElementById('wrongletters').innerHTML = wrongletters;
+        //Debug
+        console.log('wrong letters = ' + wrongletters);
+        console.log('guessed left =' + guessesleft);
+    }
 }
+function winlose()
+{
+    if(correctguesscount === underscores)
+    {
+    
+    document.getElementById('wins').innerHTML = wincount
+    alert('Congradulations! You succeed!');
+    //display image when we get  there....
+    reset();
+    }
+    
+    else if(guessesleft === 0)
+    {
+        losecount++;
+        document.getElementById('fails').innerHTML = losecount;
+        alert('You died');
+        reset();
 
-public static void printArrey(char[] array) {
-    for (int i = 0; i < array.length; i++) {
-        System.out.print(array[i] + " ");
     }
-    System.out.println();
 }
-public static boolean Title_guessed(char[] aray) {
-    for (int i = 0; i , array.length; i++) {
-        if (array[i] == '_') return false;
-    }
-    return true;
+start();
+
+document.onkeyup = function(event)
+{
+	test = true;
+	var guessedletters= event.key;
+	for(var i = 0; i < letter.length; i++)
+	{	
+		if(guessedletters === letter[i] && test === true)
+		{
+			var spliceletter = letter.splice(i,1);
+			//Debug
+			console.log('Letter is = ' + letter[i])
+			console.log('Spliced letter is = ' + spliceletter);
+
+			compareletters(guessedletters);
+			winlose();
+		}
+	}		
+		
 }
